99eda994877a7aa9d7294ad832fcf03b0928a3a4
László Kovács <atomantinator@gmail.com>
2024-10-16 11:12:01 +0200

    test #3

--- Tests/FeatherStorageDriverLocalTests/FeatherStorageDriverLocalTests.swift
+++ Tests/FeatherStorageDriverLocalTests/FeatherStorageDriverLocalTests.swift
@@ -16,6 +16,23 @@ import XCTest
 
 final class FeatherStorageDriverLocalTests: XCTestCase {
 
+    func _testList(_ storage: StorageComponent) async throws {
+        let key1 = "dir02/dir03"
+        try await storage.create(key: key1)
+
+        let key2 = "dir02/test-01.txt"
+        let data = Data("test".utf8)
+        try await storage.upload(
+            key: key2,
+            buffer: .init(data: data)
+        )
+
+        let res = try await storage.list(key: "dir02")
+        guard res == ["dir03", "test-01.txt"] else {
+            fatalError()
+        }
+    }
+    
     func testLocalDriverUsingTestSuite() async throws {
 
         let threadPool = NIOThreadPool(numberOfThreads: 1)
@@ -37,6 +54,7 @@ final class FeatherStorageDriverLocalTests: XCTestCase {
 
             let storage = try await registry.storage()
             let suite = StorageTestSuite(storage)
+            try await _testList(storage)
             try await suite.testAll()
         }
         catch {
